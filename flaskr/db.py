fc0d99d06376e34f6a07ef74322182451833f76f
Hideaki <kthd12123@icloud.com>
2022-05-22 16:00:09 +0900

    fix2_form

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,6 +4,7 @@ DATABASE = "database.db"
 
 def connect(sql,commit=False):
     con = sqlite3.connect(DATABASE)
+    con.cursor()
     db_data = con.execute(sql)
     if commit:
         con.commit()
