d8992095e24589ab46f4972e00888a33df905029
Hideaki <kthd12123@icloud.com>
2022-05-22 18:05:28 +0900

    sqlalchemy

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+class History(db.Model):
+    __tablename__ = "history"
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Text)
+    title = db.Column(db.Text)
+    document = db.Column(db.Text)
+db.create_all()
+
 import flaskr.main
 
-from flaskr import db
-db.connect("CREATE TABLE IF NOT EXISTS history (date, title, document)")
+
+#from flaskr import db
+#db.connect("CREATE TABLE IF NOT EXISTS history (date, title, document)")
--- flaskr/main.py
+++ flaskr/main.py
@@ -1,5 +1,6 @@
-from flaskr import app, db
+from flaskr import app, History, db
 from flask import render_template, url_for, request, redirect
+
 
 @app.route("/")    # [現在のURL+"/"]と通信時、実行
 def index():
@@ -7,7 +8,8 @@ def index():
         "index.html",
         historys = [
             {"date":row[0],"title":row[1],"document":row[2]}
-                for row in db.connect("SELECT * FROM history ORDER BY date ASC")
+#                for row in db.connect("SELECT * FROM history ORDER BY date ASC")
+                for row in History.query.all()
                 ]
     )
 
@@ -22,10 +24,16 @@ def add_history():
     date = request.form.get("date",default="????")
     title = request.form.get("title",default="????")
     document = request.form.get("document",default="")
-    db.connect("INSERT INTO history VALUES (?,?,?)",[date,title,document],commit=True)
+#    db.connect("INSERT INTO history VALUES (?,?,?)",[date,title,document],commit=True)
+    history = History(date=date, title=title,document=document)
+    db.session.add(history)
+    db.session.commit()
     return redirect(url_for("index"))
 
-@app.route("/delhistory/<date><title>")
+@app.route("/delhistory/<id>")
 def del_history(date,title):
-    db.connect(f"DELETE FROM history WHERE date = '{date}' AND title = '{title}'",commit=True)
+#    db.connect(f"DELETE FROM history WHERE date = '{date}' AND title = '{title}'",commit=True)
+    user = History.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
     return redirect(url_for("index"))
